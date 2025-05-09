2c072d96d1ffb35a34b86441e7e229d6ebff54ee
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-05-09 21:30:23 +0600

    text animation covered

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
-const videoSection = document.querySelector('.videoSec');
+const playCursr = () => {
+  const videoSection = document.querySelector(".videoSec");
 
-videoSection.addEventListener('mouseenter', ()=>{
-    gsap.to('.videoSec span', {
-        scale: 1,
-        opacity: 1,
-        duration: 0.5,
-    })
-})
-videoSection.addEventListener('mouseleave', ()=>{
-    gsap.to('.videoSec span', {
-        scale: 0.4,
+  videoSection.addEventListener("mouseenter", () => {
+    gsap.to(".videoSec span", {
+      scale: 1,
+      opacity: 1,
+      duration: 0.5,
+    });
+  });
+  videoSection.addEventListener("mouseleave", () => {
+    gsap.to(".videoSec span", {
+      scale: 0.4,
+      opacity: 0,
+      duration: 0.5,
+    });
+  });
+
+  videoSection.addEventListener("mousemove", (e) => {
+    gsap.to(".videoSec span", {
+      left: e.x - 50,
+      top: e.y - 50,
+      duration: 0.5,
+    });
+  });
+};
+const headerText = ()=>{
+    gsap.from('.headerM span',{
+        y: 100,
         opacity: 0,
-        duration: 0.5,
+        duration: 1.5,
+        stagger: 0.2,
+        ease: "power3.out",
     })
-})
+}
 
-videoSection.addEventListener('mousemove', (e)=>{
-    gsap.to('.videoSec span', {
-        left: e.x - 50,
-        top: e.y - 50,
-        duration: 0.5,
-    })
-})
+
+
+headerText();
+playCursr();
